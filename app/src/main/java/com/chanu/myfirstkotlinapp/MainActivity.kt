435712c29924e2729a918b82646ef885283d9d84
chanukaHub <chanukadil1999@gmail.com>
2020-11-21 16:33:28 +0530

    Finish Simple App

--- app/src/main/java/com/chanu/myfirstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/chanu/myfirstkotlinapp/MainActivity.kt
@@ -2,10 +2,22 @@ package com.chanu.myfirstkotlinapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+    }
+    fun onSubmit(view: View) {
+        val inputYear = findViewById<EditText>(R.id.yearEditText)
+        val txtAge = findViewById<TextView>(R.id.ageTextView)
+
+        val currentYear = Calendar.getInstance().get(Calendar.YEAR)
+        txtAge.text="Age :" + (currentYear - inputYear.text.toString().toInt())
     }
 }
